906b70b5314a00fc090444d7fa8323814f4dd8fb
Development Of FareezOnzz (Dev1Fonzz) <pfareezonzz01@gmail.com>
2026-02-16 03:24:42 +0800

    Update SCRIPT_URL to new Google Apps Script URL

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,5 @@
 // Ganti dengan URL Apps Script anda selepas deploy
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyopaUkM6gAjQobBuo6aNw3XfPh5CtPnsEzc1zJUDIaJ7GH46FuIJ-ZzcDDrJQifWxL/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwfo4qOl_OhcYSjzvowY1Rqy9vFNCEJyeYYja_Jksw7eIvHh0KMvK9E2zcPUYo5XbXV/exec";
 function hashPassword(password) {
   return CryptoJS.SHA256(password).toString();
 }
