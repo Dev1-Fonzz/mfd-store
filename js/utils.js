d2ad01c2e0f1fc538ee3cb69b3a4203987a3a987
Development Of FareezOnzz (Dev1Fonzz) <pfareezonzz01@gmail.com>
2026-02-16 02:52:37 +0800

    Update SCRIPT_URL to new deployment URL

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,5 @@
 // Ganti dengan URL Apps Script anda selepas deploy
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwZ6eMcZebeKyRB3GS0goN3asN1Sc-tHIQV3giJK8ZcmaoANFQoot2A-8Oc-VvymbDn/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyopaUkM6gAjQobBuo6aNw3XfPh5CtPnsEzc1zJUDIaJ7GH46FuIJ-ZzcDDrJQifWxL/exec";
 
 function hashPassword(password) {
   return CryptoJS.SHA256(password).toString();
